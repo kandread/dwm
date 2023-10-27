7c8ef089377f321e5d9f676ad1775a3bf4da2709
Kostas Andreadis <kandread@umass.edu>
2023-10-27 15:25:40 -0400

    Fixed syntax error

--- dwm.c
+++ dwm.c
@@ -435,16 +435,16 @@ buttonpress(XEvent *e)
                 focus(NULL);
         }
         if (ev->window == selmon->barwin) {
-                i = x = 0;
-                unsigned int occ = 0;
-                for(c = m->clients; c; c=c->next)
-                  occ |= c->tags;
-                /* Do not reserve space for vacant tags */
-                if (!(occ & 1 << i || m->tagset[m->seltags] & 1 << i))
-                  continue;
-                do
-                  x += TEXTW(tags[i]);
-        } while (ev->x >= x && ++i < LENGTH(tags));
+          i = x = 0;
+          unsigned int occ = 0;
+          for(c = m->clients; c; c=c->next)
+            occ |= c->tags;
+          do {
+            /* Do not reserve space for vacant tags */
+            if (!(occ & 1 << i || m->tagset[m->seltags] & 1 << i))
+              continue;
+            x += TEXTW(tags[i]);
+          } while (ev->x >= x && ++i < LENGTH(tags));
         if (i < LENGTH(tags)) {
           click = ClkTagBar;
           arg.ui = 1 << i;
